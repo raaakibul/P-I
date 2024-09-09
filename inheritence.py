88db897fb9dbdc3bb927e1a05eb7b317a43b60d2
rakibul-islam <raakibul.uap@gmail.com>
2024-09-09 21:21:02 +0600

    inheritence file and code added

---
+++ inheritence.py
@@ -0,0 +1,29 @@
+# inheritence 
+
+class Person:
+    
+    def __init__(self, name, age, id):
+        self.name = name
+        self.age = age
+        self.id = id
+        
+    def __str__(self):
+        return "Name: {},Age: {},Id: {}".format(self.name, self.age, self.id)
+    
+    def get_older(self,years):
+        self.age += years
+
+class Worker(Person):
+    
+    def __init__(self, name, age, id, salary):
+        super(Worker, self).__init__(name, age, id, salary)
+        self.salary = salary       
+    
+    
+    def calc_yearly_salary(self, salary):
+        return self.salary * 12
+    
+worker1 = Worker("Henry", 40, 145, 3000)
+print(worker1)
+print(worker1.calc_yearly_salary()) 
+    
