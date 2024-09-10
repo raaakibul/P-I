6b9daeb32ce07d7ad72ac8f1233f7faebd824070
rakibul-islam <raakibul.uap@gmail.com>
2024-09-10 21:38:37 +0600

    multithreading

--- multithreading.py
+++ multithreading.py
@@ -3,5 +3,19 @@ import threading
 def hello_world():
     print("Hello World")
     
+def function():
+    for x in range(100):
+        print("One")
+def function2():
+    for x in range(100):
+        print("Two")
+    
 t1 = threading.Thread(target=hello_world)
 t1.start()
+
+t2 = threading.Thread(target=function)
+t3 = threading.Thread(target=function2)
+
+t2.start()
+t3.start()
+
