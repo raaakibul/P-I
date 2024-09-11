deefb58e6f3be4a196a6a10ea5999a49c0d0d20e
rakibul-islam <raakibul.uap@gmail.com>
2024-09-11 22:27:33 +0600

    Threads

--- Threads.py
+++ Threads.py
@@ -13,14 +13,18 @@ def double():
         time.sleep(1)
         print(x)
     print("Reached the maximum")
+    lock.release()
 
 def half():
-    global x
+    global x, lock
+    lock.acquire()
     while x> 1:
         x /=2
         print(x)
         time.sleep(1)
     print("Reached the minimum")
+    lock.release()
+    
 t1 = threading.Thread(target=half)
 t2 = threading.Thread(target=double)
 t1.start()
