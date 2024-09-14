1af50323c66baec9f72a8d571cb52771e6590631
rakibul-islam <raakibul.uap@gmail.com>
2024-09-15 00:19:18 +0600

    queues code added

--- Queues.py
+++ Queues.py
@@ -1,2 +1,13 @@
 # Data structure 
 import queue
+
+# numbers = [1,2,3,4,5,6,7,8]
+# counter = 1
+
+q = queue.Queue()
+numbers = [10,20,30,40,50,60]
+for number in numbers:
+    q.put(number)
+    # print(q.get())
+print(q.get())
+print(q.get())
