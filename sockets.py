34fb4c05ec9f8a9c85d194c70847a566ca0ab710
rakibul-islam <raakibul.uap@gmail.com>
2024-09-15 22:36:25 +0600

    socket code added

--- sockets.py
+++ sockets.py
@@ -2,3 +2,17 @@
 # communication channels
 # use for internet
 import socket
+
+# internet socket
+# TCP protocol 
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(('127.0.0.1', 55555))
+
+s.listen()
+
+while True:
+    client, address = s.accept()
+    print("Connecting to {}".format(address))
+    client.send("You are connected {}".endcode())
+    client.close()
+    
