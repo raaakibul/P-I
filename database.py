1eed578b59ff23cdfb4f6175fd06798eebaef795
rakibul-islam <raakibul.uap@gmail.com>
2024-09-16 23:51:09 +0600

    create database

--- database.py
+++ database.py
@@ -1,3 +1,27 @@
 import sqlite3
 
 connection = sqlite3.connect("mydata.db")
+
+cursor = connection.cursor()
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS persons(
+    first_name TEXT,
+    last_name TEXT,
+    age INTEGER
+)
+
+""")
+
+cursor.execute("""
+INSERT INTO persons VALUES
+('Paul', 'Smith', 24).
+('Ana', 'Isabela',25),
+('John', 'Ball', 28)
+
+""")
+
+
+connection.commit()
+
+connection.close()
