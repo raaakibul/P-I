5805a51f0b400e938db49da710e07106f4b5cbeb
rakibul-islam <raakibul.uap@gmail.com>
2024-09-17 22:12:50 +0600

    recursion code added

---
+++ recursion.py
@@ -0,0 +1,19 @@
+#non recursion way 
+n = 7
+
+fact = 1
+
+while n>0:
+    fact = fact * n
+    n = n - 1
+print(fact)
+
+# recursion way
+
+def factorial(x):
+    if x <1:
+        return 1
+    else: 
+        number = x * factorial(x-1)
+        return number
+    
